868cfba10a3914fee26d1736f651c0144d4828f4
cksgud0403 <cksgud0403@naver.com>
2024-09-05 20:46:27 +0900

    feat: 자바 예외 이해 - 언체크 예외 활용

---
+++ src/test/java/com/example/jdbc/exception/basic/UncheckedAppTest.java
@@ -0,0 +1,86 @@
+package com.example.jdbc.exception.basic;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+@Slf4j
+public class UncheckedAppTest {
+
+
+    @Test
+    void unchecked() {
+        Controller controller = new Controller();
+
+        Assertions.assertThatThrownBy(() -> controller.request()).isInstanceOf(RuntimeException.class);
+    }
+
+
+    static class Controller {
+         Service service = new Service();
+
+        public void request() {
+            service.logic();
+        }
+    }
+
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() {
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+
+    static class NetworkClient {
+        public void call() {
+            throw new RuntimeConnectExpection("연결 실패");
+        }
+    }
+
+
+    static class Repository {
+        public void call() {
+            try {
+                runSQL();
+            }catch (SQLException e) {
+                throw new RuntimeSQLExpection(e);
+            }
+        }
+
+
+
+
+        private void runSQL() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+
+
+    static class RuntimeConnectExpection extends RuntimeException {
+        public RuntimeConnectExpection(String message) {
+            super(message);
+        }
+    }
+
+
+    static class RuntimeSQLExpection extends RuntimeException {
+        public RuntimeSQLExpection() {
+
+        }
+
+        public RuntimeSQLExpection(Throwable cause) {
+            super(cause);
+        }
+
+    }
+
+}
